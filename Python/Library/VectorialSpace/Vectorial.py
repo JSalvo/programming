014ee9df6280298e06f761d3e40c381d33bf03b0
Gianmario Salvetti <gianmario.salvetti@gmail.com>
2014-06-24 09:01:26 +0200

    Visual correction

--- Python/Library/VectorialSpace/Vectorial.py
+++ Python/Library/VectorialSpace/Vectorial.py
@@ -12,6 +12,7 @@ class Point3d:
 		self.__v[i] = self.__v[i] * self.__v[3]
 	def __sub__(self, p2):
 		return Vector3d([self[0] - p2[0], self[1] - p2[1], self[2] - p2[2]])
+	# Return a Vector3d object representing "self" point
 	def to3dVector(self):
 		result = Vector3d()
 		result[0] = self[0]
@@ -89,21 +90,24 @@ class Vector:
 			result[i] = self[i] / float( b )
 		
 		return result
-	# Product scalar per Vector
+	# Product Vector per scalar s
 	def scalarPerVector(self, s):
 		result = Vector(len(self))
 		for i in range(0, len(self)):
 			result[i] = result[i] * s
+	# Return a copy of self object
 	def clone(self):
 		result = Vector(len(self), self)
 		return result
 			
 	def __repr__(self):
 		return self.__v.__repr__()
-	def __normalize__(self):
-		val = math.sqrt(self*self)
-		result = self / val
-		return result
+	# Return a normalized version of self vector (unity lenght vector)
+	def __normalize__(self):		
+		return self / self.length()		
+	# Return self vector' lenght 
+	def length(self):
+		return math.sqrt(self*self)
 	
 class Vector3d(Vector):
 	def __init__(self, v=None):
@@ -118,7 +122,52 @@ class Vector3d(Vector):
 		return result
 	def clone(self):
 		result = Vector3d(self)
+	
 		return result
+
+class MatrixIncompatibleDimension(Exception):
+	def __init__(self, column, row):
+		self.__column = column
+		self.__row = row
+
+class Matrix():
+	def __init__(self, row, column):
+		self.__row = row
+		self.__column = column
+		self.__m = []
+		for i in range(0, row):
+			self.__m.append([])
+			for j in range(0, column):
+				self.__m[i].append(0)
+	def row(self):
+		return self.__row
+		
+	def column(self):
+		return self.__column
+	
+	def __mul__(self, m):
+		result = Matrix(self.row(), m.column())
+		
+		if self.column() == m.row():
+			for i in range(0, self.row()):				
+				for j in range(0, m.column()):
+					tmp = 0
+					for k in range(0, self.column()):
+						tmp = tmp + self[i][k] * m[k][j]
+					print tmp
+					
+					result[i][j] = tmp
+		else:
+			raise MatrixIncompatibleDimension(self.column(), m.row())
+		
+		return result
+		
+	def __getitem__(self, i):
+		return self.__m[i]
+	def __repr__(self):
+		return self.__m.__repr__()
+				
+
 	
 """ Test 1
 v1 = Vector(3, [1,2,3])
@@ -134,6 +183,7 @@ z = x ** y
 
 print(z)"""
 
+"""
 p1 = Point3d([0, 0, 0])
 print p1
 p2 = Point3d([1, 0, 0])
@@ -150,6 +200,32 @@ print v3
 p4 = Point3d(v3)
 
 print p4
+"""
+
+m1 = Matrix(2,3)
+m2 = Matrix(3,2)
+
+m1[0][0] = 1
+m1[0][1] = 2
+m1[0][2] = 3
+
+m1[1][0] = 3
+m1[1][1] = 4
+m1[1][2] = 5
+
+m2[0][0] = 1
+m2[1][0] = 3
+m2[2][0] = 5
+
+
+m2[0][1] = 2
+m2[1][1] = 4
+m2[2][1] = 8
+
+m = m1*m2
+
+print m
+
 
 
 
